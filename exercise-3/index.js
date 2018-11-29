44191e11d40d46ab9adb8d18c4ea6d42b9fcce6f
Jose Luis Represa <joseluis.represa@bbva.com>
2018-11-29 17:33:32 +0100

    housekeeping

--- exercise-3/index.js
+++ exercise-3/index.js
@@ -1,12 +1,4 @@
-const fs = require('fs');
-const path = require('path');
 const http = require('http');
-
-// const filepath = path.resolve(__dirname, '..', 'package.json');
-// const package = fs.readFileSync(filepath, 'utf-8');
-// const json = JSON.parse(package);
-
-// console.log(`The version is ${json.version}`);
 
 http.createServer((req, res) => {
   const { url: pathname } = req;
